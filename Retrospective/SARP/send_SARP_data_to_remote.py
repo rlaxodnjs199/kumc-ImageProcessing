d1095484c994d593518c5c60f7c90bb27ab0f8fe
rlaxodnjs199 <rlaxodnjs199@gmail.com>
2021-10-27 09:06:32 -0500

    update syntax

--- Retrospective/SARP/send_SARP_data_to_remote.py
+++ Retrospective/SARP/send_SARP_data_to_remote.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List
 import pandas as pd
 import tarfile
 from datetime import datetime
@@ -12,46 +12,67 @@ from dotenv import dotenv_values
 QCTCFD_WORKSHEET_PATH = r"E:\common\Taewon\oneDrive\OneDrive - University of Kansas Medical Center\QCTCFD_Worksheet.xlsx"
 VIDA_RESULTS_PATH = r"E:\VIDA\VIDAvision2.2"
 OUTPUT_PATH = "Data_to_send"
-OUTPUT_FOLDER = "VIDA_20211026-26_SARP3_TK"
+OUTPUT_FOLDER = "VIDA_20211026-26_SARP3_T1_TK"
 PATH_IN_REMOTE_HOST = "/home/twkim/ImageData"
 # ---------------------------------------------------------------------------
+
 
 def pick_baseline_with_both_InEx(df_QCTCFD: pd.DataFrame) -> List[List[int]]:
     VIDA_case_id_list = []
     for _, row in df_QCTCFD.iterrows():
-        if row['VIDA_IN'] == 'O' and row['VIDA_EX'] == 'O' and row['FU'] == 0:
-            VIDA_case_id_list.append(int(row['VIDA_IN_Path'].split('\\')[-1]))
-            VIDA_case_id_list.append(int(row['VIDA_EX_Path'].split('\\')[-1]))
-    splitted_VIDA_case_id_list = np.array_split(VIDA_case_id_list, 9)
-    
+        if row["VIDA_IN"] == "O" and row["VIDA_EX"] == "O" and row["FU"] == 1:
+            VIDA_case_id_list.append(int(row["VIDA_IN_Path"].split("\\")[-1]))
+            VIDA_case_id_list.append(int(row["VIDA_EX_Path"].split("\\")[-1]))
+    splitted_VIDA_case_id_list = np.array_split(VIDA_case_id_list, 6)
+
     return splitted_VIDA_case_id_list
 
-def compress_vida_results(case_ids: List[int], index: int, vida_results_path=VIDA_RESULTS_PATH, output_path=OUTPUT_PATH, output_folder=OUTPUT_FOLDER):
+
+def compress_vida_results(
+    case_ids: List[int],
+    index: int,
+    vida_results_path=VIDA_RESULTS_PATH,
+    output_path=OUTPUT_PATH,
+    output_folder=OUTPUT_FOLDER,
+):
     with tarfile.open(f"{output_path}/{output_folder}_{index}.tar.bz2", "w:bz2") as tar:
         for case in tqdm(case_ids):
             tar.add(Path(f"{vida_results_path}/{case}"), f"{output_folder}/{case}")
-    
+
     return f"{output_path}/{output_folder}_{index}.tar.bz2"
+
 
 def prepare_ProjSubjList(df_QCTCFD: pd.DataFrame, case_ids: List[int], index: int):
     df_row_to_append_list = []
     for case_id in case_ids:
         df_row_to_append = {}
-        df_row_to_append['Proj'] = 'SARP'
-        if (df_QCTCFD['VIDA_IN_Path'].str.contains(str(case_id))).any():
-            df_QCTCFD_row_filtered = df_QCTCFD[df_QCTCFD['VIDA_IN_Path'].str.contains(str(case_id), na=False)]
+        df_row_to_append["Proj"] = "SARP"
+        if (df_QCTCFD["VIDA_IN_Path"].str.contains(str(case_id))).any():
+            df_QCTCFD_row_filtered = df_QCTCFD[
+                df_QCTCFD["VIDA_IN_Path"].str.contains(str(case_id), na=False)
+            ]
             # df_row_to_append['Proj'] = df_QCTCFD_row_filtered['Proj'].values[0]
-            df_row_to_append['Subj'] = df_QCTCFD_row_filtered['Subj'].values[0]
-            df_row_to_append['Img'] = 'IN' + df_QCTCFD_row_filtered['FU'].astype('str').values[0]
-            df_row_to_append['ImgDir'] = f'{PATH_IN_REMOTE_HOST}/{OUTPUT_FOLDER}_{index}/{case_id}'
-        elif (df_QCTCFD['VIDA_EX_Path'].str.contains(str(case_id))).any():
-            df_QCTCFD_row_filtered = df_QCTCFD[df_QCTCFD['VIDA_EX_Path'].str.contains(str(case_id), na=False)]
-            # df_row_to_append['Proj'] = df_QCTCFD_row_filtered['Proj'].values[0]            
-            df_row_to_append['Subj'] = df_QCTCFD_row_filtered['Subj'].values[0]
-            df_row_to_append['Img'] = 'EX' + df_QCTCFD_row_filtered['FU'].astype('str').values[0]
-            df_row_to_append['ImgDir'] = f'{PATH_IN_REMOTE_HOST}/{OUTPUT_FOLDER}_{index}/{case_id}'
+            df_row_to_append["Subj"] = df_QCTCFD_row_filtered["Subj"].values[0]
+            df_row_to_append["Img"] = (
+                "IN" + df_QCTCFD_row_filtered["FU"].astype("str").values[0]
+            )
+            df_row_to_append[
+                "ImgDir"
+            ] = f"{PATH_IN_REMOTE_HOST}/{OUTPUT_FOLDER}_{index}/{case_id}"
+        elif (df_QCTCFD["VIDA_EX_Path"].str.contains(str(case_id))).any():
+            df_QCTCFD_row_filtered = df_QCTCFD[
+                df_QCTCFD["VIDA_EX_Path"].str.contains(str(case_id), na=False)
+            ]
+            # df_row_to_append['Proj'] = df_QCTCFD_row_filtered['Proj'].values[0]
+            df_row_to_append["Subj"] = df_QCTCFD_row_filtered["Subj"].values[0]
+            df_row_to_append["Img"] = (
+                "EX" + df_QCTCFD_row_filtered["FU"].astype("str").values[0]
+            )
+            df_row_to_append[
+                "ImgDir"
+            ] = f"{PATH_IN_REMOTE_HOST}/{OUTPUT_FOLDER}_{index}/{case_id}"
         else:
-            print('error')
+            print("error")
         df_row_to_append_list.append(df_row_to_append)
     df_ProjSubjList = pd.DataFrame(df_row_to_append_list)
 
@@ -59,10 +80,10 @@ def prepare_ProjSubjList(df_QCTCFD: pd.DataFrame, case_ids: List[int], index: in
     df_to_write = df_ProjSubjList.to_csv(index=False, line_terminator="\n").replace(
         ",", "    "
     )
-    with open(f'{OUTPUT_PATH}/ProjSubjList.in.{today}_{index}', 'w') as f:
+    with open(f"{OUTPUT_PATH}/ProjSubjList.in.{today}_T1_{index}", "w") as f:
         f.write(df_to_write)
-    
-    return f'{OUTPUT_PATH}/ProjSubjList.in.{today}_{index}'
+
+    return f"{OUTPUT_PATH}/ProjSubjList.in.{today}_T1_{index}"
 
 
 def send_data_to_B2(ProjSubjList_path: str, VIDA_result_path: str):
@@ -71,17 +92,16 @@ def send_data_to_B2(ProjSubjList_path: str, VIDA_result_path: str):
     host = ssh_config["SSH_HOST"]
     user = ssh_config["SSH_USERNAME"]
 
-
     subprocess.run(
-            [
-                "scp",
-                "-P",
-                port,
-                f"{ProjSubjList_path}",
-                f"{user}@{host}:{PATH_IN_REMOTE_HOST}/",
-            ]
-        )
-    
+        [
+            "scp",
+            "-P",
+            port,
+            f"{ProjSubjList_path}",
+            f"{user}@{host}:{PATH_IN_REMOTE_HOST}/",
+        ]
+    )
+
     subprocess.run(
         [
             "scp",
@@ -93,11 +113,10 @@ def send_data_to_B2(ProjSubjList_path: str, VIDA_result_path: str):
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     df_QCTCFD = pd.read_excel(QCTCFD_WORKSHEET_PATH)
     splitted_VIDA_case_id_list = pick_baseline_with_both_InEx(df_QCTCFD)
     for i, list in enumerate(splitted_VIDA_case_id_list):
-        if i == 2:
-            ProjSubjList_path = prepare_ProjSubjList(df_QCTCFD, list, i)
-            VIDA_result_path = compress_vida_results(list, i)
-            send_data_to_B2(ProjSubjList_path, VIDA_result_path)
+        ProjSubjList_path = prepare_ProjSubjList(df_QCTCFD, list, i)
+        VIDA_result_path = compress_vida_results(list, i)
+        send_data_to_B2(ProjSubjList_path, VIDA_result_path)
--- Retrospective/SARP/update_QCTCFD_Worksheet.py
+++ Retrospective/SARP/update_QCTCFD_Worksheet.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 import pandas as pd
 from openpyxl import load_workbook
 
@@ -7,34 +6,67 @@ VIDASHEET_PATH = r"E:\common\Taewon\oneDrive\OneDrive - University of Kansas Med
 MASTERSHEET_PATH = r"P:\IRB_STUDY00144315_AATRU\Jiwoong\SARP\Data\SARP_DCC\SARP_ClusterCT\SARP_I_II_and_III_master_data_report _04112019_DL.xlsx"
 QCTCFD_WORKSHEET_PATH = r"E:\common\Taewon\oneDrive\OneDrive - University of Kansas Medical Center\QCTCFD_Worksheet.xlsx"
 
+
 def extract_subj(subj):
-    return subj.split('_')[0]
+    return subj.split("_")[0]
+
 
 def extract_FU(subj):
     return int(subj[-1]) - 1
 
-def trim_subj(subj):
-    return '-'.join(subj.split('-')[:-1])
 
-def construct_df_QCTCFD_from_VIDA_dashboard(df_QCTCFD: pd.DataFrame, df_VIDA: pd.DataFrame):
+def trim_subj(subj):
+    return "-".join(subj.split("-")[:-1])
+
+
+def construct_df_QCTCFD_from_VIDA_dashboard(
+    df_QCTCFD: pd.DataFrame, df_VIDA: pd.DataFrame
+):
     for _, row in df_VIDA.iterrows():
-        if row['CT Protocol'].split(' ')[0] == 'INSPIRATION':
-            df_QCTCFD.loc[(df_QCTCFD['Subj'] == row['Subj']) & (df_QCTCFD['Date'] == row['ScanDate']), 'VIDA_IN'] = 'O'
-            df_QCTCFD.loc[(df_QCTCFD['Subj'] == row['Subj']) & (df_QCTCFD['Date'] == row['ScanDate']), 'VIDA_IN_Path'] = f'{VIDA_RESULT_PATH}\{row.VidaCaseID}'
-            df_QCTCFD.loc[(df_QCTCFD['Subj'] == row['Subj']) & (df_QCTCFD['Date'] == row['ScanDate']), 'VIDA_IN_At'] = 'KUMC'
-        elif row['CT Protocol'].split(' ')[0] == 'EXPIRATION':
-            df_QCTCFD.loc[(df_QCTCFD['Subj'] == row['Subj']) & (df_QCTCFD['Date'] == row['ScanDate']), 'VIDA_EX'] = 'O'
-            df_QCTCFD.loc[(df_QCTCFD['Subj'] == row['Subj']) & (df_QCTCFD['Date'] == row['ScanDate']), 'VIDA_EX_Path'] = f'{VIDA_RESULT_PATH}\{row.VidaCaseID}'
-            df_QCTCFD.loc[(df_QCTCFD['Subj'] == row['Subj']) & (df_QCTCFD['Date'] == row['ScanDate']), 'VIDA_EX_At'] = 'KUMC'
+        if row["CT Protocol"].split(" ")[0] == "INSPIRATION":
+            df_QCTCFD.loc[
+                (df_QCTCFD["Subj"] == row["Subj"])
+                & (df_QCTCFD["Date"] == row["ScanDate"]),
+                "VIDA_IN",
+            ] = "O"
+            df_QCTCFD.loc[
+                (df_QCTCFD["Subj"] == row["Subj"])
+                & (df_QCTCFD["Date"] == row["ScanDate"]),
+                "VIDA_IN_Path",
+            ] = f"{VIDA_RESULT_PATH}\{row.VidaCaseID}"
+            df_QCTCFD.loc[
+                (df_QCTCFD["Subj"] == row["Subj"])
+                & (df_QCTCFD["Date"] == row["ScanDate"]),
+                "VIDA_IN_At",
+            ] = "KUMC"
+        elif row["CT Protocol"].split(" ")[0] == "EXPIRATION":
+            df_QCTCFD.loc[
+                (df_QCTCFD["Subj"] == row["Subj"])
+                & (df_QCTCFD["Date"] == row["ScanDate"]),
+                "VIDA_EX",
+            ] = "O"
+            df_QCTCFD.loc[
+                (df_QCTCFD["Subj"] == row["Subj"])
+                & (df_QCTCFD["Date"] == row["ScanDate"]),
+                "VIDA_EX_Path",
+            ] = f"{VIDA_RESULT_PATH}\{row.VidaCaseID}"
+            df_QCTCFD.loc[
+                (df_QCTCFD["Subj"] == row["Subj"])
+                & (df_QCTCFD["Date"] == row["ScanDate"]),
+                "VIDA_EX_At",
+            ] = "KUMC"
+
 
 def append_df_to_excel(
-    filename,
-    df,
-    sheet_name="Sheet1",
-    startrow=None,
-    **to_excel_kwargs
+    filename, df, sheet_name="Sheet1", startrow=None, **to_excel_kwargs
 ):
-    with pd.ExcelWriter(filename, engine="openpyxl", mode="a", date_format='m/d/yyyy', datetime_format='m/d/yyyy') as writer:
+    with pd.ExcelWriter(
+        filename,
+        engine="openpyxl",
+        mode="a",
+        date_format="m/d/yyyy",
+        datetime_format="m/d/yyyy",
+    ) as writer:
         writer.book = load_workbook(filename)
         startrow = writer.book[sheet_name].max_row
         if startrow is None:
@@ -42,22 +74,28 @@ def append_df_to_excel(
         writer.sheets = {ws.title: ws for ws in writer.book.worksheets}
         df.to_excel(writer, sheet_name, startrow=startrow, **to_excel_kwargs)
 
+
 # DF: SARP3 available in VIDA_machine:/e/jchoi4/ImageData/SARP/VIDA_20201102-03_SARP_VIDA
-def update_QCTCFD_WORKSHEET_columns(filename,
-    df,
-    sheet_name="Sheet1",
-    **to_excel_kwargs
+def update_QCTCFD_WORKSHEET_columns(
+    filename, df, sheet_name="Sheet1", **to_excel_kwargs
 ):
-    with pd.ExcelWriter(filename, engine="openpyxl", mode="a", date_format='m/d/yyyy', datetime_format='m/d/yyyy') as writer:
+    with pd.ExcelWriter(
+        filename,
+        engine="openpyxl",
+        mode="a",
+        date_format="m/d/yyyy",
+        datetime_format="m/d/yyyy",
+    ) as writer:
         writer.book = load_workbook(filename)
         writer.sheets = {ws.title: ws for ws in writer.book.worksheets}
         df.to_excel(writer, sheet_name, startrow=0, **to_excel_kwargs)
 
+
 # DF: SARP3 imported to VIDA
 df_VIDA = pd.read_excel(VIDASHEET_PATH)
-df_SARP3 = df_VIDA.loc[df_VIDA['Subj'].str.contains('80-'), :]
-df_SARP3['Subj'] = df_SARP3['Subj'].apply(extract_subj)
-df_SARP3['ScanDate'] = pd.to_datetime(df_SARP3['ScanDate'], format='%Y%m%d')
+df_SARP3 = df_VIDA.loc[df_VIDA["Subj"].str.contains("80-"), :]
+df_SARP3["Subj"] = df_SARP3["Subj"].apply(extract_subj)
+df_SARP3["ScanDate"] = pd.to_datetime(df_SARP3["ScanDate"], format="%Y%m%d")
 # df_SARP3_unique_group = df_SARP3.groupby(['Subj', 'ScanDate']).size().reset_index().rename(columns={0:'count'})
 
 # DF: SARP3 available in B2
@@ -73,14 +111,24 @@ df_SARP3['ScanDate'] = pd.to_datetime(df_SARP3['ScanDate'], format='%Y%m%d')
 # QCTCFD_Worksheet DF
 df_MASTER = pd.read_excel(MASTERSHEET_PATH)
 df_QCTCFD = pd.read_excel(QCTCFD_WORKSHEET_PATH)
-df_MASTER_SARP3 = df_MASTER.loc[df_MASTER['EDF_ID'].str.contains('80-'), :][['EDF_ID', 'study_date']]
-df_MASTER_SARP3['FU'] = df_MASTER_SARP3['EDF_ID'].apply(extract_FU)
-df_MASTER_SARP3['EDF_ID'] = df_MASTER_SARP3['EDF_ID'].apply(trim_subj)
-df_MASTER_SARP3['study_date'] = pd.to_datetime(df_MASTER_SARP3['study_date'], format='%Y%m%d')
-df_MASTER_SARP3.rename(columns={'EDF_ID':'Subj', 'study_date': 'Date'}, inplace=True)
-df_MASTER_SARP3_unique = df_MASTER_SARP3.groupby(['Subj', 'Date', 'FU']).size().reset_index().rename(columns={0:'count'}).drop(columns='count')
-df_MASTER_SARP3_unique['Proj'] = 'SARP3'
-df_QCTCFD = pd.concat([df_QCTCFD, df_MASTER_SARP3_unique]).fillna('')
+df_MASTER_SARP3 = df_MASTER.loc[df_MASTER["EDF_ID"].str.contains("80-"), :][
+    ["EDF_ID", "study_date"]
+]
+df_MASTER_SARP3["FU"] = df_MASTER_SARP3["EDF_ID"].apply(extract_FU)
+df_MASTER_SARP3["EDF_ID"] = df_MASTER_SARP3["EDF_ID"].apply(trim_subj)
+df_MASTER_SARP3["study_date"] = pd.to_datetime(
+    df_MASTER_SARP3["study_date"], format="%Y%m%d"
+)
+df_MASTER_SARP3.rename(columns={"EDF_ID": "Subj", "study_date": "Date"}, inplace=True)
+df_MASTER_SARP3_unique = (
+    df_MASTER_SARP3.groupby(["Subj", "Date", "FU"])
+    .size()
+    .reset_index()
+    .rename(columns={0: "count"})
+    .drop(columns="count")
+)
+df_MASTER_SARP3_unique["Proj"] = "SARP3"
+df_QCTCFD = pd.concat([df_QCTCFD, df_MASTER_SARP3_unique]).fillna("")
 
 construct_df_QCTCFD_from_VIDA_dashboard(df_QCTCFD, df_SARP3)
 # append_df_to_excel(QCTCFD_WORKSHEET_PATH, df_QCTCFD, header=None, index=False)
