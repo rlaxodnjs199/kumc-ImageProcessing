dbacd8182562870b0aa653ca805e7a0297c824e5
rlaxodnjs199 <rlaxodnjs199@gmail.com>
2022-04-25 21:13:13 -0500

    [Fix] Count files - use index

--- Util/extract_series_description.py
+++ Util/extract_series_description.py
@@ -21,6 +21,8 @@ dcm_dict: Dict = {}
 for base, _, files in os.walk(src_path):
     for file in tqdm(files, total=num_files, initial=last_state):
         folder_name = os.path.basename(base)
+        subj = folder_name.split("_")[0]
+        ctdate = folder_name.split("_")[1]
         file_path = os.path.join(base, file)
         dcm = dcmread(file_path)
         if hasattr(dcm, "SliceThickness"):
@@ -33,10 +35,18 @@ for base, _, files in os.walk(src_path):
             if series_description not in dcm_dict[folder_name]:
                 dcm_dict[folder_name].append(slice_thickness)
                 dcm_dict[folder_name].append(series_description)
+                dcm_dict[folder_name].append(1)
+            else:
+                count_index = dcm_dict[folder_name].index(series_description) + 1
+                try:
+                    dcm_dict[folder_name][count_index] += 1
+                except:
+                    print(series_description, dcm_dict[folder_name][count_index])
         else:
-            dcm_dict[folder_name] = [slice_thickness, series_description]
+            dcm_dict[folder_name] = [slice_thickness, series_description, 1]
 
     last_state += len(files)
 
 df_dcm_dict = pd.DataFrame.from_dict(dcm_dict, orient="index")
-df_dcm_dict.to_csv("./output/series_descriptions.csv", header=False)
+print(df_dcm_dict)
+df_dcm_dict.to_csv("./output/series_descriptions_v2.csv", header=False)
